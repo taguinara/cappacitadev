8309db41d4511a51daa88c02fc22ad5a981d0e04
Tainara Almeida <68087649+taguinara@users.noreply.github.com>
2021-08-23 00:05:10 -0300

    Update atividade_2.js

--- atividade_2.js
+++ atividade_2.js
@@ -1,1 +1,18 @@
-MEU ARQUIVO
+/*atividade do módulo 1*/
+const listaClientes = [{
+    nome: "Larissa",
+    cidade: "Sumaré"
+},
+{
+    nome: "Hugo",
+    cidade: "São Vicente"
+},
+{
+    nome: "André",
+    cidade: "Osasco"
+}
+]
+
+for (var i = 0; i < listaClientes.length; i++){
+console.log(listaClientes[i].cidade)
+}
