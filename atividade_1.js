80098d50a6affd40803fe23b06b7d01eb714b534
Tainara Almeida <68087649+taguinara@users.noreply.github.com>
2021-08-23 00:04:40 -0300

    Update atividade_1.js

--- atividade_1.js
+++ atividade_1.js
@@ -1,1 +1,8 @@
-MEU ARQUIVO
+/*atividade do módulo 1*/
+const idade = 18 
+
+if (idade >= 18){
+    console.log("Você é obrigado a votar!")
+} else{
+    console.log("Você não é obrigado a votar!")
+}
