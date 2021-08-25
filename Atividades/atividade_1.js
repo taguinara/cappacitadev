99b7304b8298788fa46bfc717f67979725758bd0
Tainara Almeida <68087649+taguinara@users.noreply.github.com>
2021-08-25 01:33:17 -0300

    Add files via upload

---
+++ Atividades/atividade_1.js
@@ -0,0 +1,8 @@
+/*atividade do módulo 1*/
+const idade = 18 
+
+if (idade >= 18){
+    console.log("Você é obrigado a votar!")
+} else{
+    console.log("Você não é obrigado a votar!")
+}
---
+++ Atividades/atividade_3.js
@@ -0,0 +1,6 @@
+/*atividade do módulo 1*/
+const listaDeDoces = ['barra de chocolate', 'bombom', 'bala de goma', 'cocada']
+
+listaDeDoces.pop()
+
+console.log(listaDeDoces)
